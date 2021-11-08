03cf835cc404ea4f9dd857ec38dd3523d41ead27
risingphoenixx <pandasonali568@gmail.com>
2021-11-08 15:10:32 +0530

    updated

---
+++ ecom-app-frontend-main/src/app/config/my-app-config.ts
@@ -0,0 +1,8 @@
+export default {
+    oidc: {
+        clientId: '0oapmjdu0NyzFSGrq5d6Aj',
+        issuer: 'https://dev-261897788.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid','profile','email']
+    }
+}
